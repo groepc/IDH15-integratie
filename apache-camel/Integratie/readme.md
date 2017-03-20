aca37980929abe08ac981a0583deacd47f7ccce9
Vadiem Janssens <vadiem.janssens@webcolorsgroup.com>
2017-03-20 10:22:39 +0100

    renamed MainExample to App

---
+++ apache-camel/Integratie/readme.md
@@ -0,0 +1,2 @@
+    cd ./Integratie
+    mvn compile exec:java -Dexec.mainClass=org.groepc.MainExample
--- apache-camel/Integratie/src/main/java/org/groepc/App.java
+++ apache-camel/Integratie/src/main/java/org/groepc/App.java
@@ -1,46 +1,101 @@
 package org.groepc;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
+import org.apache.camel.Main;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.MainListenerSupport;
+import org.apache.camel.main.MainSupport;
 
-/**
- * Hello world!
- */
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        // create CamelContext
-        CamelContext context = new DefaultCamelContext();
+	private Main main;
 
-        // add our route to the CamelContext
-        context.addRoutes(new RouteBuilder() {
-            public void configure() {
+	public static void main(String[] args) throws Exception {
+		App example = new App();
+		example.boot();
+	}
 
-                
-                from("timer://myTimer?period=2000")
-                        .setBody()
-                        .simple("Hello World Camel fired at ${header.firedTime}")
-                        .to("stream:out");
+	public void boot() throws Exception {
+		// create a Main instance
+		main = new Main();
+		// bind MyBean into the registry
+		main.bind("mySqlBean", new MySqlBean());
+		// add routes
+		main.addRouteBuilder(new MyRouteBuilder());
+		// add event listener
+		main.addMainListener(new Events());
+		// set the properties from a file
+//		main.setPropertyPlaceholderLocations("example.properties");
+		// run until you terminate the JVM
+		System.out.println("Starting Camel. Use ctrl + c to terminate the JVM.\n");
+		main.run();
+	}
 
-                from("timer://foo?fixedRate=true&delay=0&period=10000")
-                        .to("https://randomuser.me/api/")
-                        .setHeader(Exchange.FILE_NAME, constant("message.html"))
-                        //.to("stream:out");
-                        .to("file:target");
+	private static class MyRouteBuilder extends RouteBuilder {
+		private Properties prop = new Properties();
 
-            }
-        });
+		@Override
+		public void configure() throws Exception {
 
-        // start the route and let it do its work
-        context.start();
+			// load a properties file
+			InputStream input = new FileInputStream("config.properties");
+			this.prop.load(input);
 
-        Thread.sleep(10000);
+			from("scheduler://defaultTimer?delay=5&timeUnit=SECONDS").to("bean:mySqlBean?method=requestDb");
 
-        // stop the CamelContext
-        context.stop();
 
-        System.out.println("== Done ==");
-    }
+//			from("timer://myTimer?period=2000")
+//					.setBody()
+//					.simple("Hello World Camel fired at ${header.firedTime}")
+//					.to("stream:out");
+
+//			from("timer://myTimer?period=5000")
+//					.setHeader("subject", simple("Duimen omhoog!!"))
+//					.to("smtp://apikey@smtp.sendgrid.net:587?password=" + sendgridApi + "&to=" + toMail + "&from=camellover@gmail.com")
+//					.to("stream:out");
+
+//			String sendgridApi = prop.getProperty("sendgrid_api");
+//			String toMail = prop.getProperty("to_mail");
+//
+//			from("timer://foo?fixedRate=true&delay=0&period=10000")
+//					.to("https://randomuser.me/api/")
+//					.setHeader(Exchange.FILE_NAME, constant("message.html"))
+//					//.to("stream:out");
+//					.to("file:target");
+
+//			from("timer:foo?delay=1000")
+//					.process(new Processor() {
+//						public void process(Exchange exchange) throws Exception {
+//							System.out.println("Invoked timer at " + new Date());
+//						}
+//					})
+//					.bean("foo");
+		}
+	}
+
+	public static class MySqlBean {
+		public void callMe() {
+			System.out.println("MyBean.callMe method has been called");
+		}
+
+		public void requestDb() {
+			System.out.println("MySqlBean.requestDB method has been called");
+		}
+	}
+
+	public static class Events extends MainListenerSupport {
+
+		@Override
+		public void afterStart(MainSupport main) {
+			System.out.println("MainExample with Camel is now started!");
+		}
+
+		@Override
+		public void beforeStop(MainSupport main) {
+			System.out.println("MainExample with Camel is now being stopped!");
+		}
+	}
 }
