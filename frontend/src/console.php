0e0984fda38ce28d7238cb39347135720e42ff25
Perry Faro <perryfaro@live.nl>
2017-03-24 21:44:37 +0100

    Console werkend gemaakt

--- frontend/src/console.php
+++ frontend/src/console.php
@@ -6,18 +6,30 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-$console = new Application('My Silex Application', 'n/a');
+$console = new Application('Is het fiets weer', 'n/a');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console->setDispatcher($app['dispatcher']);
 $console
-    ->register('my-command')
-    ->setDefinition(array(
-        // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
-    ))
-    ->setDescription('My command description')
-    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-        // do something
-    })
-;
+        ->register('trigger-camel')
+        ->setDefinition(array(
+                // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
+        ))
+        ->setDescription('Triggers camel to send e-mails based on the actuel weather')
+        ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+
+
+            $notifications = $app['db']->fetchAll("SELECT * FROM notifications WHERE DATE_FORMAT(concat_ws(' ', CURDATE(), notification_time ), '%H:%i')=DATE_FORMAT(NOW(), '%H:%i')");
+            print_r($notifications);
+            foreach ($notifications as $notification) {
+                if (!$notification) {
+                    return new Response('Invalid hash.', 500);
+                }
+
+                $url = 'http://' . getenv('CAMEL_HOST') . ':' . getenv('CAMEL_PORT') . '/api/notify';
+                $headers = array('Content-Type' => 'application/json', 'Accept' => 'application/json');
+                $response = Requests::post($url, $headers, json_encode($notification));
+                print_r($response);
+            }
+        });
 
 return $console;
